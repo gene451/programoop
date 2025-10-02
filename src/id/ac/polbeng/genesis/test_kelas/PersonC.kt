47e03e30482ad2f40e5e192565ecf9662559cca2
GENESIS <genesisgilbert45@gmail.com>
2025-10-02 22:30:52 +0700

    6PersonC

---
+++ src/id/ac/polbeng/genesis/test_kelas/PersonC.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.genesis.test_kelas
+class PersonC (var firstName: String, var lastName: String, var age: Int) {}
+fun main(){
+    val budi = PersonC("Budi", "Gunawan", 21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+}
